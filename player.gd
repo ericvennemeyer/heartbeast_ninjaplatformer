5e2c3f4a0ed5fdb5c330b7599b5721be3833a668
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-07 11:19:08 -0700

    P5 - Attack Animation

--- player.gd
+++ player.gd
@@ -5,6 +5,18 @@ extends CharacterBody2D
 @onready var animation_player_lower: AnimationPlayer = $AnimationPlayerLower
 
 
+func _ready() -> void:
+	animation_player_lower.current_animation_changed.connect(func(anim_name: String):
+		if animation_player_upper.current_animation == "attack": return
+		animation_player_upper.play(anim_name)
+		)
+	animation_player_upper.animation_finished.connect(func(anim_name: String):
+		if anim_name != "attack": return
+		animation_player_upper.play(animation_player_lower.current_animation)
+		animation_player_upper.seek(animation_player_lower.current_animation_position)
+		)
+
+
 func _physics_process(delta: float) -> void:
 	var x_input = Input.get_axis("ui_left", "ui_right")
 	
@@ -14,18 +26,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = -200
 	
+	if Input.is_action_just_pressed("ui_accept"):
+		animation_player_upper.play("attack")
+	
 	velocity.x = x_input * 80
 	
 	if x_input == 0:
-		animation_player_upper.play("stand")
 		animation_player_lower.play("stand")
 	else:
 		anchor.scale.x = sign(x_input)
-		animation_player_upper.play("run")
 		animation_player_lower.play("run")
 	
 	if not is_on_floor():
-		animation_player_upper.play("jump")
 		animation_player_lower.play("jump")
 	
 	move_and_slide()
