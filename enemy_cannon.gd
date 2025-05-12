3fcb1017024d3286d3549c898b59598795897c56
Eric Vennemeyer <henri.barun@gmail.com>
2025-05-12 12:15:33 -0700

    P12 - Stats Resource

--- enemy_cannon.gd
+++ enemy_cannon.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+@export var stats: Stats:
+	set(value):
+		stats = value
+		if stats is not Stats: return
+		stats = stats.duplicate()
+
 @onready var hurtbox: Hurtbox = $Hurtbox
 
 
 func _ready() -> void:
 	hurtbox.hurt.connect(func(other_hitbox: Hitbox):
+		stats.health -= other_hitbox.damage
+	)
+	stats.no_health.connect(func(): 
 		queue_free()
 	)
--- hitbox.gd
+++ hitbox.gd
@@ -10,5 +10,4 @@ func _ready() -> void:
 func _on_area_entered(area_2d: Area2D) -> void:
 	assert(area_2d is Hurtbox, "The Hitbox detected an area that is not a Hurtbox.")
 	var hurtbox = area_2d as Hurtbox
-	if hurtbox.is_invincible: return
-	hurtbox.hurt.emit(self)
+	hurtbox.take_hit(self)
